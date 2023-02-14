10c7068ab03b5b78da838dabe0dafd8c328568aa
asadm11 <mahmood.asad467@gmail.com>
2023-02-14 23:00:56 +0530

    parsing incoming request

--- app.js
+++ app.js
@@ -5,22 +5,38 @@ const http = require('http');
 
 //importing express
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //The express module export a function, so, we're calling the express function and storing its return value in the app constant
 const app = express();
+
+app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 
 //expressjs allows us to have various middleware functions between the request and the response
 //use method allows us to add a new middleware function
 //It accepts an array of request handlers
 //next is a function that allows the user to travel to the next middleware
-app.use((req, res, next) => {
-    console.log("This is the middleware!");
-    next();
+//use also accepts a path for routing
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.use('/add-product', (req, res, next) => {
+    console.log("This is another middleware!!");
+    res.send(`<form action='/product' method='POST'> <input type='text' name='title'> <button type='submit'>Send</button></form>`);
 });
 
-app.use((req, res, next) => {
-    console.log("This is another middleware!!");
-    res.send(`<h1> Response from express!!</h1> `);
+//instead of 'use' we can use 'post' which works only in case of post request 
+app.post('/product', (req, res, next) => {
+    console.log("In another middleware!");
+    console.log(req.body);
+    res.redirect("/");
+});
+
+app.use('/', (req, res, next) => {
+    res.send(`<h1> Hey There!</h1>`);
+    console.log("Hello expressJS!");
 });
 
 
@@ -28,6 +44,7 @@ app.use((req, res, next) => {
 
 //Use the createServer() method to create an HTTP server:
 //whenever a request is made, a callback function is called which includes the request and response as two arguments
+
 // const server = http.createServer(app);       :No need for this in expressjs, app.listen will do it for us
 
 //The server object listens on port mentioned(5500)
