ebffe2160d372f81af147bb804615292c489d371
asadm11 <mahmood.asad467@gmail.com>
2023-02-26 19:58:15 +0530

    Refactoring the file storage code and completing the mvc architecture

--- models/product.js
+++ models/product.js
@@ -1,6 +1,25 @@
 const fs = require("fs");
 const path = require("path");
 
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      //calling the callback function
+      cb([]);
+    } else {
+        //calling the callback function
+        cb(JSON.parse(fileContent));
+    }
+    
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
@@ -8,40 +27,20 @@ module.exports = class Product {
 
   save() {
     // products.push(this);
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);     //json.parse converts the json data into javascript
-      }
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {      //stringify converts the javascript data into json data
-        console.log(err);
-      });
-    });
-  }
+    getProductsFromFile(products => {
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          //stringify converts the javascript data into json data
+          console.log(err);
+        });
+    });      
+    };
 
   //static keyword makes the function dependent upon the class and not an object of the class
   //cb is a callback function passed as an argument in fetchall which is invoked when the fetchAll function completes it's process
   //we need to do this because readFile is an async process
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        //calling the callback function
-        cb([]);
-      }
-      //calling the callback function
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
     // return products;
   }
 };
